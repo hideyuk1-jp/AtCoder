f60fffca5c1fe2d6f8402f0b2f6f6ca309cba68a
hideyuk1-jp <hh11028+github@gmail.com>
2020-03-18 18:31:00 +0900

    ABC147 Dも解説見てAC

--- php/abc147.php
+++ php/abc147.php
@@ -2,20 +2,49 @@
 // バーチャル参加でCまでAC 600 69:22 => 推定パフォーマンス999
 
 define('MOD', 10 ** 9 + 7);
-
 fscanf(STDIN, '%d', $n);
 $a = array_map('intval', explode(' ', trim(fgets(STDIN))));
 $ans = 0;
-for ($i = 0; $i < $n - 1; $i++) {
-    for ($j = $i + 1; $j < $n; $j++) {
-        $ans = modAdd($ans,  (int) ($a[$i] ^ $a[$j]));
+for ($k = 0; $k < 60; $k++) {
+    $cnt_1 = 0;
+    for ($i = 0; $i < $n; $i++) {
+        if (($a[$i] >> $k) & 1) $cnt_1++;
     }
+    $ans = modAdd($ans, modMul(modMul($n - $cnt_1, $cnt_1), modPow(2, $k)));
 }
 echo $ans . PHP_EOL;
 
+// 足し算
 function modAdd($x, $y)
 {
     return ($x + $y) % MOD;
+}
+
+// 引き算
+function modSub($x, $y)
+{
+    return ($x + MOD - $y) % MOD;
+}
+
+// 掛け算
+function modMul($x, $y)
+{
+    return ($x * $y) % MOD;
+}
+
+// 割り算
+function modDiv($x, $y)
+{
+    return modMul($x, modPow($y, MOD - 2));
+}
+
+// 累乗（繰り返し二乗法）
+function modPow($n, $x)
+{
+    if ($x === 0) return 1;
+    $res = (modPow($n, $x >> 1) ** 2) % MOD;
+    if ($x % 2 === 1) $res = modMul($res, $n);
+    return $res;
 }
 
 exit;
