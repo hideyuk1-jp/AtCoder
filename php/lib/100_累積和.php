abfe2ac0c353bf60d58d323f04ba2073ec8a86c9
hideyuk1-jp <hh11028+github@gmail.com>
2020-03-20 15:11:50 +0900

    libの累積和修正

--- php/lib/100_累積和.php
+++ php/lib/100_累積和.php
@@ -3,16 +3,23 @@ fscanf(STDIN, '%d', $n);
 $a = array_map('intval', explode(' ', trim(fgets(STDIN))));
 
 // 累積和
-$sum = array_fill(0, $n + 1, 0);
-for ($i = 0; $i < $n; $i++) {
-    $sum[$i + 1] = $sum[$i] + $a[$i];
-}
-
-echo implode(' ', $sum) . PHP_EOL;
+$cusum = cusum($a);
+echo implode(' ', $cusum) . PHP_EOL;
 
 // 区間 [left, right) の総和を求める
 fscanf(STDIN, '%d %d', $left, $right);
-echo '区間 [left, right) の総和: ' . ($sum[$right] - $sum[$left]) . PHP_EOL;
+echo '区間 [left, right) の総和: ' . ($cusum[$right] - $cusum[$left]) . PHP_EOL;
 
 // 区間 [left, right] の総和を求める
-echo '区間 [left, right] の総和: ' . ($sum[$right + 1] - $sum[$left]) . PHP_EOL;
+echo '区間 [left, right] の総和: ' . ($cusum[$right + 1] - $cusum[$left]) . PHP_EOL;
+
+// 累積和
+function cusum($a)
+{
+    $n = count($a);
+    $res = array_fill(0, $n + 1, 0);
+    for ($i = 0; $i < $n; $i++) {
+        $res[$i + 1] = $res[$i] + $a[$i];
+    }
+    return $res;
+}
