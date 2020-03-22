1acd3dde61e18fc63d51aa3367b31c62b9020f28
hideyuk1-jp <hh11028+github@gmail.com>
2020-03-22 11:47:05 +0900

    ABC116 AB（CはAC済み）

--- php/abc116.php
+++ php/abc116.php
@@ -1,11 +1,38 @@
 <?php
+// B
+fscanf(STDIN, '%d', $s);
+$n = 1000000;
+$a = $s;
+$c[$s] = true;
+for ($i = 2; $i <= $n; $i++) {
+    $a = ($a & 1) ? 3 * $a + 1 : $a / 2;
+    if (isset($c[$a])) break;
+    $c[$a] = true;
+}
+echo $i;
+
+function f($n)
+{
+    return ($n & 1) ? 3 * $n + 1 : $n / 2;
+}
+
+exit;
+
+// A
+fscanf(STDIN, '%d %d %d', $ab, $bc, $ca);
+echo $ab * $bc / 2;
+
+exit;
+
+// C
 fscanf(STDIN, '%d', $n);
 $h = array_map('intval', explode(' ', trim(fgets(STDIN))));
 
 $ans = func($h);
 echo $ans . PHP_EOL;
 
-function func($h, $_min = 0) {
+function func($h, $_min = 0)
+{
     $min = max($h);
     $j = 0;
     foreach ($h as $i => $v) {
