832fb26fe0e63302c4b9620c9bd3d3480dd1a4a0
hideyuk1-jp <hh11028+github@gmail.com>
2020-03-25 08:58:26 +0900

    ABC113 ABをAC

--- php/abc113.php
+++ php/abc113.php
@@ -1,4 +1,24 @@
 <?php
+// B
+fscanf(STDIN, '%d', $n);
+fscanf(STDIN, '%d %d', $t, $a);
+$h = array_map('intval', explode(' ', trim(fgets(STDIN))));
+$min = [-1, 10 ** 6];
+for ($i = 0; $i < $n; $i++) {
+    $d = abs($a - ($t - $h[$i] * 0.006));
+    if ($min[1] > $d) {
+        $min = [$i, $d];
+    }
+}
+echo $min[0] + 1;
+
+// A
+fscanf(STDIN, '%d %d', $x, $y);
+echo ($x + $y / 2);
+
+exit;
+
+// C
 fscanf(STDIN, '%d %d', $n, $m);
 for ($i  = 0; $i < $m; $i++) {
     fscanf(STDIN, '%d %d', $_p, $y[]);
@@ -7,9 +27,9 @@ for ($i  = 0; $i < $m; $i++) {
 }
 array_multisort($p, $y, $c);
 $j = 1;
-for($i = 0; $i < $m; $i++) {
+for ($i = 0; $i < $m; $i++) {
     if (($p[$i - 1] ?? $p[$i]) !== $p[$i]) $j = 1;
     $num[$c[$i]] = str_pad($p[$i] + 1, 6, 0, STR_PAD_LEFT) . str_pad($j, 6, 0, STR_PAD_LEFT);
     $j++;
 }
-for($i = 0; $i < $m; $i++) echo $num[$i] . PHP_EOL;
+for ($i = 0; $i < $m; $i++) echo $num[$i] . PHP_EOL;
