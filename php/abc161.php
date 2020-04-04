1ad4df02ed4d224a9ef22746283f4f416154bee7
hideyuk1-jp <hh11028+github@gmail.com>
2020-04-05 02:37:48 +0900

    ABC161 Fを自力AC

--- php/abc161.php
+++ php/abc161.php
@@ -1,5 +1,124 @@
 <?php
 // コンテスト参加 Dまで自力AC 1000 22:24 => パフォ 1060
+// F
+fscanf(STDIN, '%d', $n);
+$ans = 0;
+
+// N-1が倍数になるパターン
+$divisors = divisors($n - 1);
+$ans += count($divisors) - 1;
+
+// Nが倍数になるパターン
+$divisors = divisors($n);
+// var_dump($divisors);
+foreach ($divisors as $d) {
+    if ($d === 1) continue;
+    $n2 = $n;
+    while ($n2 % $d === 0) {
+        $n2 = intdiv($n2, $d);
+    }
+    if ($n2 % $d === 1) $ans++;
+}
+
+echo $ans;
+
+/**
+ * エラトステネスのふるい
+ * $maxまでの整数のうち素数のみを格納した配列を返す
+ */
+function primesArr($max)
+{
+    $is_prime = isPrimeArr($max);
+    $res = [];
+    for ($i = 1; $i <= $max; $i++) {
+        if ($is_prime[$i]) $res[] = $i;
+    }
+    return $res;
+}
+
+/**
+ * エラトステネスのふるい
+ * $maxまでの整数が素数かどうかboolを格納した配列を返す
+ */
+function isPrimeArr($max)
+{
+    $arr = array_fill(0, $max + 1, true);
+    $arr[0]  = $arr[1] = false;
+    $rmax = (int) floor(sqrt($max));
+    for ($i = 2; $i <= $rmax; $i++) {
+        for ($j = 2 * $i; $j <= $max; $j += $i) {
+            $arr[$j] = false;
+        }
+    }
+    return $arr;
+}
+
+// 素因数分解
+function primeFactorization($n)
+{
+    $res = [];
+    $res[1] = 1;
+    if ($n === 1) return $res;
+
+    $primes = primesArr((int) sqrt($n) + 1);
+
+    foreach ($primes as $prime) {
+        while ($n % $prime === 0) {
+            if (isset($res[$prime])) {
+                $res[$prime]++;
+            } else {
+                $res[$prime] = 1;
+            }
+            $n /= $prime;
+        }
+    }
+
+    if ($n > 1) {
+        if (isset($res[$n])) {
+            $res[$n]++;
+        } else {
+            $res[$n] = 1;
+        }
+    }
+
+    return $res;
+}
+
+function primes($max)
+{
+    $arr = [];
+    for ($i = 2; $i <= $max; $i++) {
+        if (isPrime($i)) $arr[] = $i;
+    }
+    return $arr;
+}
+
+function isPrime($n)
+{
+    if ($n === 1) return false;
+    $rmax = (int) floor(sqrt($n));
+    for ($i = 2; $i <= $rmax; $i++) {
+        if ($n % $i === 0) return false;
+    }
+    return true;
+}
+
+function divisors($max)
+{
+    $arr = [];
+    $rmax = (int) floor(sqrt($max));
+    for ($i = 1; $i <= $rmax; $i++) {
+        if ($max % $i === 0) {
+            $arr[] = $i;
+            $arr[] = $max / $i;
+        }
+    }
+    sort($arr);
+    return array_unique($arr);
+}
+
+exit;
+
 // E
 fscanf(STDIN, '%d %d %d', $n, $k, $c);
 fscanf(STDIN, '%s', $s);
