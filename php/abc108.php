56d66f580178b5587c68ad855b369196c8d56dc7
hideyuk1-jp <hh11028+github@gmail.com>
2020-05-22 16:08:46 +0900

    abc108のA~CをAC（Cカンニング）

---
+++ php/abc108.php
@@ -0,0 +1,20 @@
+<?php
+// C
+fscanf(STDIN, '%d %d', $n, $k);
+$ans = intdiv($n, $k) ** 3;
+if ($k % 2 === 0) $ans += intdiv($n + $k / 2, $k) ** 3;
+echo $ans;
+
+exit;
+
+// B
+fscanf(STDIN, '%d %d %d %d', $x1, $y1, $x2, $y2);
+$xx = $x2 - $x1;
+$yy = $y2 - $y1;
+echo implode(' ', [$x2 - $yy, $y2 + $xx, $x1 - $yy, $y1 + $xx]);
+
+exit;
+
+// A
+fscanf(STDIN, '%d', $k);
+echo ((int) floor($k / 2) * (int) ceil($k / 2));
