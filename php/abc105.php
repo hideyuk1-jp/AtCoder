9a30db36b1e774c2e5e0f536a5736921946f8d08
hideyuk1-jp <hh11028+github@gmail.com>
2020-05-25 21:11:49 +0900

    abc105-BC AC

--- php/abc105.php
+++ php/abc105.php
@@ -1,4 +1,35 @@
 <?php
+// C
+fscanf(STDIN, '%d', $n);
+if (!$n) exit('0');
+$b = '';
+while ($n) {
+    if ($n % 2) {
+        $b .= '1';
+        $n--;
+    } else {
+        $b .= '0';
+    }
+    $n /= -2;
+}
+echo strrev($b);
+
+exit;
+
+// B
+fscanf(STDIN, '%d', $n);
+$c = intdiv($n, 7);
+$ans = 'No';
+for ($i = 0; $i <= $c; $i++) {
+    if (($n - $i * 7) % 4 === 0) {
+        $ans = 'Yes';
+        break;
+    }
+}
+echo $ans;
+
+exit;
+
 // A
 fscanf(STDIN, '%d %d', $n, $k);
 echo $n % $k > 0 ? 1 : 0;
