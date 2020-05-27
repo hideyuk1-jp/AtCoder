96a866725ebbefe2fcdb595a2cc641f5d67fbd2b
hideyuk1-jp <hh11028+github@gmail.com>
2020-05-27 16:51:35 +0900

    abc102-ABC Cのみ解説見てAC

---
+++ php/abc102.php
@@ -0,0 +1,23 @@
+<?php
+// C
+fscanf(STDIN, '%d', $n);
+$a = array_map('intval', explode(' ', trim(fgets(STDIN))));
+for ($i = 0; $i < $n; $i++) $aa[] = $a[$i] - ($i + 1);
+sort($aa);
+$b = $aa[intdiv($n, 2)];
+$ans = 0;
+for ($i = 0; $i < $n; $i++) $ans += abs($a[$i] - ($i + 1) - $b);
+echo $ans;
+
+exit;
+
+// B
+fscanf(STDIN, '%d', $n);
+$a = array_map('intval', explode(' ', trim(fgets(STDIN))));
+echo max($a) - min($a);
+
+exit;
+
+// A
+fscanf(STDIN, '%d', $n);
+echo $n % 2 ? $n * 2 : $n;
