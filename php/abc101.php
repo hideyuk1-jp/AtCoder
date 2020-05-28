4df05550010b7d91b48df65025f4ed3b48213a1b
hideyuk1-jp <hh11028+github@gmail.com>
2020-05-28 14:46:59 +0900

    abc101-ABC 自力AC

---
+++ php/abc101.php
@@ -0,0 +1,20 @@
+<?php
+// C
+fscanf(STDIN, '%d %d', $n, $k);
+$a = array_map('intval', explode(' ', trim(fgets(STDIN))));
+echo (int) ceil(($n - 1) / ($k - 1));
+
+exit;
+
+// B
+fscanf(STDIN, '%d', $n);
+$s = array_sum(str_split(strval($n)));
+echo $n % $s ? 'No' : 'Yes';
+
+exit;
+
+// A
+fscanf(STDIN, '%s', $s);
+$ans = 0;
+for ($i = 0; $i < strlen($s); $i++) $ans += $s[$i] === '+' ? 1 : -1;
+echo $ans;
