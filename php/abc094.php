e29e45493cd178adcf93c2d90633a78e326d84af
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-04 12:10:55 +0900

    ABC094-ABCD ABCD自力AC

---
+++ php/abc094.php
@@ -0,0 +1,52 @@
+<?php
+// C
+fscanf(STDIN, '%d', $n);
+$a = array_map('intval', explode(' ', trim(fgets(STDIN))));
+sort($a);
+$l = $a[$n - 1];
+$r = -1;
+$d = PHP_INT_MAX;
+for ($i = 0; $i < $n - 1; $i++) {
+    $td = $l % 2 === 0
+        ? abs(intdiv($l, 2) - $a[$i])
+        : min(abs(intdiv($l, 2) - $a[$i]), abs(intdiv($l, 2) + 1 - $a[$i]));
+    if ($td < $d) {
+        $d = $td;
+        $r = $a[$i];
+    }
+}
+echo implode(' ', [$l, $r]);
+
+exit;
+
+// C
+fscanf(STDIN, '%d', $n);
+$x = array_map('intval', explode(' ', trim(fgets(STDIN))));
+$_x = $x;
+asort($x);
+sort($_x);
+$cnt = 0;
+foreach ($x as $i => $v) {
+    $ans[$i] = intdiv($n, 2) > $cnt ? $_x[intdiv($n, 2)] : $_x[intdiv($n, 2) - 1];
+    $cnt++;
+}
+ksort($ans);
+echo implode(PHP_EOL, $ans);
+
+exit;
+
+// B
+fscanf(STDIN, '%d %d %d', $n, $m, $x);
+$a = array_map('intval', explode(' ', trim(fgets(STDIN))));
+$c0 = $cn = 0;
+for ($i = 0; $i < $m; $i++) {
+    if ($a[$i] < $x) $c0++;
+    else $cn++;
+}
+echo min($c0, $cn);
+
+exit;
+
+// A
+fscanf(STDIN, '%d %d %d', $a, $b, $x);
+echo $x >= $a && $x <= $a + $b ? 'YES' : 'NO';
