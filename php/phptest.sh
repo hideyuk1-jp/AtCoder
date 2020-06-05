2470bc904fb76a4926d9f53005a50e8830e4c3a9
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-05 17:41:58 +0900

    サンプルテストの実行環境構築(PHP)

--- php/phptest.sh
+++ php/phptest.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 name=$1
-oj dl "https://atcoder.jp/tasks/${contest}${problemname:0:3}_${problemname:3}"
 oj dl "https://atcoder.jp/contests/${name:0:6}/tasks/${name:0:8}"
-php ./$1.php
-oj test
-rm -f a.out
+oj t -c " php ./$1.php"
 rm -rf test
