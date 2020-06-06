a911baeb41ea52f5a364b073639aed11b0f92f3f
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-06 10:41:41 +0900

    abc090_b AC

---
+++ php/abc090_b.php
@@ -0,0 +1,13 @@
+<?php
+list($a, $b) = ints();
+$cnt = 0;
+for ($i = $a; $i <= $b; $i++) {
+    $s = strval($i);
+    if ($s[0] === $s[4] && $s[1] === $s[3]) $cnt++;
+}
+echo $cnt;
+
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
