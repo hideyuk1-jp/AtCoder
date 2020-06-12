02a6c988cb0752956a01c1bee7de3f4857bebbee
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-12 15:39:54 +0900

    lib ダイクストラを修正（クラス化）

--- php/lib/220_ダイクストラ.php
+++ php/lib/220_ダイクストラ.php
@@ -1,13 +1,20 @@
 <?php
 // abc079_d
 list($h, $w) = ints();
+$d = new Dijkstra(10);
 for ($i = 0; $i < 10; ++$i)
-    foreach (ints() as $j => $v) $cost[$i][$j] = $v;
-for ($i = 0; $i < 10; ++$i) $d[$i] = dijkstra($i, 10);
+    foreach (ints() as $j => $v) $d->connect($i, $j, $v);
+for ($i = 0; $i < 10; ++$i) $d->solve($i);
 $ans = 0;
 for ($i = 0; $i < $h; ++$i)
-    foreach (ints() as $v) $ans += $v !== -1 ? $d[$v][1] : 0;
-echo $ans;
+    foreach (ints() as $v) $ans += $v !== -1 ? $d->dist($v, 1) : 0;
+echo $ans . PHP_EOL;
+// 各ノードから１までの
+for ($i = 0; $i < 10; ++$i) {
+    echo "Shortest root from ${i} to 1: ";
+    echo implode(" -> ", $d->root($i, 1));
+    echo " (" . $d->dist($i, 1) . ")" . PHP_EOL;
+}
 
 function ints()
 {
@@ -15,28 +22,56 @@ function ints()
 }
 
 // ノードsからの各ノードへの最短距離
-function dijkstra($s, $n)
+// 0->n-1
+class Dijkstra
 {
-    global $cost, $root;
-    $d = array_fill(0, $n, INF);
-    $d[$s] = 0;
-    $root[$s] = array_fill(0, $n, [$s]); // 各ノードまでの最短経路を格納
-    $used = array_fill(0, $n, false);
-    while (true) {
-        $v = -1;
-        // 残りのノードのうち最短で到達出来るノードを選択
-        for ($i = 0; $i < $n; ++$i) {
-            if (!$used[$i] && $v === -1) $v = $i;
-            elseif (!$used[$i] && $d[$i] < $d[$v]) $v = $i;
-        }
-        if ($v === -1) break;
-        for ($i = 0; $i < $n; ++$i) {
-            if ($d[$v] + $cost[$v][$i] < $d[$i]) {
-                $d[$i] = $d[$v] + $cost[$v][$i];
-                $root[$s][$i] = array_merge($root[$s][$v], [$i]);
-            }
-        }
-        $used[$v] = true;
+    private $n;
+    private $d; // 最短距離を格納
+    private $cost; // ノード間の距離を格納
+    private $root; // 最短経路を格納
+
+    function __construct($n)
+    {
+        $this->n = $n;
+        $this->cost = array_fill(0, $this->n, array_fill(0, $this->n, INF));
     }
-    return $d;
+
+    function connect($x, $y, $w)
+    {
+        $this->cost[$x][$y] = $w;
+    }
+
+    function solve($s = 0)
+    {
+        $this->d[$s] = array_fill(0, $this->n, INF);
+        $this->d[$s][$s] = 0;
+        $this->root[$s] = array_fill(0, $this->n, [$s]); // 各ノードまでの最短経路を格納
+        $used = array_fill(0, $this->n, false);
+        while (true) {
+            $v = -1;
+            // 残りのノードのうち最短で到達出来るノードを選択
+            for ($i = 0; $i < $this->n; ++$i) {
+                if (!$used[$i] && $v === -1) $v = $i;
+                elseif (!$used[$i] && $this->d[$s][$i] < $this->d[$s][$v]) $v = $i;
+            }
+            if ($v === -1) break;
+            for ($i = 0; $i < $this->n; ++$i) {
+                if ($this->d[$s][$i] > $this->d[$s][$v] + $this->cost[$v][$i]) {
+                    $this->d[$s][$i] = $this->d[$s][$v] + $this->cost[$v][$i];
+                    $this->root[$s][$i] = array_merge($this->root[$s][$v], [$i]);
+                }
+            }
+            $used[$v] = true;
+        }
+    }
+
+    function dist($x, $y)
+    {
+        return $this->d[$x][$y];
+    }
+
+    function root($x, $y)
+    {
+        return $this->root[$x][$y];
+    }
 }
