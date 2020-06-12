194303b533d5d76076833083fd679446eb125f75
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-12 15:47:04 +0900

    lib ワーシャルフロイドを修正（クラス化）

--- php/lib/240_ワーシャルフロイド.php
+++ php/lib/240_ワーシャルフロイド.php
@@ -2,12 +2,13 @@
 // ワーシャルフロイド
 // abc079_d
 list($h, $w) = ints();
+$wf = new WarshallFloyd(10);
 for ($i = 0; $i < 10; ++$i)
-    foreach (ints() as $j => $v) $d[$i][$j] = $v;
-warshallFloyd(10);
+    foreach (ints() as $j => $v) $wf->connect($i, $j, $v);
+$wf->solve();
 $ans = 0;
 for ($i = 0; $i < $h; ++$i)
-    foreach (ints() as $v) $ans += $v !== -1 ? $d[$v][1] : 0;
+    foreach (ints() as $v) $ans += $v !== -1 ? $wf->dist($v, 1) : 0;
 echo $ans;
 
 function ints()
@@ -15,14 +16,35 @@ function ints()
     return array_map('intval', explode(' ', trim(fgets(STDIN))));
 }
 
-function warshallFloyd($n)
+class WarshallFloyd
 {
-    global $d;
-    for ($k = 0; $k < $n; ++$k) { // 経由点
-        for ($i = 0; $i < $n; ++$i) { // 始点
-            for ($j = 0; $j < $n; ++$j) { // 終点
-                $d[$i][$j] = min($d[$i][$j], $d[$i][$k] + $d[$k][$j]);
+    private $n;
+    private $d;
+
+    function __construct($n)
+    {
+        $this->n = $n;
+        $this->d = array_fill(0, $this->n, array_fill(0, $this->n, INF));
+    }
+
+    function connect($x, $y, $w)
+    {
+        $this->d[$x][$y] = $w;
+    }
+
+    function solve()
+    {
+        for ($k = 0; $k < $this->n; ++$k) { // 経由点
+            for ($i = 0; $i < $this->n; ++$i) { // 始点
+                for ($j = 0; $j < $this->n; ++$j) { // 終点
+                    $this->d[$i][$j] = min($this->d[$i][$j], $this->d[$i][$k] + $this->d[$k][$j]);
+                }
             }
         }
+    }
+
+    function dist($x, $y)
+    {
+        return $this->d[$x][$y];
     }
 }
