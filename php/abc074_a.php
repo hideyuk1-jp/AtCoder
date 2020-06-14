a4893e9ac650623f1af3fa8b4fe49af7665bf429
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-14 15:29:32 +0900

    abc074_abc 自力AC

---
+++ php/abc074_a.php
@@ -0,0 +1,8 @@
+<?php
+list($n) = ints();
+list($a) = ints();
+echo $n * $n - $a;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
---
+++ php/abc074_b.php
@@ -0,0 +1,11 @@
+<?php
+list($n) = ints();
+list($k) = ints();
+$x = ints();
+$ans = 0;
+for ($i = 0; $i < $n; ++$i) $ans += 2 * min($k - $x[$i], $x[$i]);
+echo $ans;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
