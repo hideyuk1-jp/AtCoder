a7acfa1a8228b9ec6e44a3df11a46ce839f30283
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-15 21:44:50 +0900

    abc064_abcd 自力AC

---
+++ php/abc064_b.php
@@ -0,0 +1,8 @@
+<?php
+list($n) = ints();
+$a = ints();
+echo max($a) - min($a);
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
---
+++ php/abc064_d.php
@@ -0,0 +1,32 @@
+<?php
+list($n) = ints();
+list($s) = strs();
+$cnt['('] = 0;
+$c = 0;
+for ($i = 0; $i < $n; ++$i) {
+    if ($s[$i] === '(') $c++;
+    else $c--;
+    if ($c < 0) {
+        $cnt['(']++;
+        $c = 0;
+    }
+}
+$cnt[')'] = 0;
+$c = 0;
+for ($i = $n - 1; $i >= 0; --$i) {
+    if ($s[$i] === ')') $c++;
+    else $c--;
+    if ($c < 0) {
+        $cnt[')']++;
+        $c = 0;
+    }
+}
+echo str_repeat('(', $cnt['(']) . $s . str_repeat(')', $cnt[')']);
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
