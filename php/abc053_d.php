43e54af3f572361abb45e20ce37cb7f8779b7331
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-17 02:38:00 +0900

    abc053_d リファクタリング

--- php/abc053_d.php
+++ php/abc053_d.php
@@ -1,13 +1,9 @@
 <?php
 list($n) = ints();
 $a = ints();
-for ($i = 0; $i < $n; ++$i) {
-    if (isset($cnt[$a[$i]])) $cnt[$a[$i]]++;
-    else $cnt[$a[$i]] = 1;
-}
-$c = 0;
-foreach ($cnt as $v) $c += $v - 1;
-echo $c % 2 ? count($cnt) - 1 : count($cnt);
+for ($i = 0; $i < $n; ++$i) $cnt[$a[$i]] = true;
+$k = count($cnt);
+echo ($n - $k) % 2 ? $k - 1 : $k;
 function ints()
 {
     return array_map('intval', explode(' ', trim(fgets(STDIN))));
