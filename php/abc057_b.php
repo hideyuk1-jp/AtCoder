29ebb79a483efc5120751c8c74ffc0f05590261a
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-16 13:36:26 +0900

    abc057_abc 自力AC

---
+++ php/abc057_b.php
@@ -0,0 +1,21 @@
+<?php
+list($n, $m) = ints();
+for ($i = 0; $i < $n; ++$i) $a[] = ints();
+for ($i = 0; $i < $m; ++$i) $c[] = ints();
+for ($i = 0; $i < $n; ++$i) {
+    $min_d = PHP_INT_MAX;
+    $min_j = 0;
+    for ($j = 0; $j < $m; ++$j) {
+        $d = abs($c[$j][0] - $a[$i][0]) + abs($c[$j][1] - $a[$i][1]);
+        if ($d < $min_d) {
+            $min_d = $d;
+            $min_j = $j;
+        }
+    }
+    $ans[] = $min_j + 1;
+}
+echo implode(PHP_EOL, $ans);
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
---
+++ php/abc057_c.php
@@ -0,0 +1,13 @@
+<?php
+list($n) = ints();
+$ans = PHP_INT_MAX;
+for ($i = 1; $i <= (int) sqrt($n); ++$i) {
+    if ($n % $i) continue;
+    $f = max(strlen((string) $i), strlen((string) intdiv($n, $i)));
+    $ans = min($ans, $f);
+}
+echo $ans;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
