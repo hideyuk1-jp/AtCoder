190922b215061d66e25d72239d122b0c92489a74
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-17 16:21:01 +0900

    abc043_abc 自力AC

---
+++ php/abc043_c.php
@@ -0,0 +1,16 @@
+<?php
+list($n) = ints();
+$a = ints();
+$ans = PHP_INT_MAX;
+for ($i = -100; $i <= 100; ++$i) {
+    $c = 0;
+    for ($j = 0; $j < $n; ++$j) {
+        $c += pow($i - $a[$j], 2);
+    }
+    $ans = min($ans, $c);
+}
+echo $ans;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
