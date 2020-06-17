49dcd6d06d8be0f94f9987048bcc3623f7990876
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-17 14:50:45 +0900

    abc046_d 自力AC

---
+++ php/abc046_d.php
@@ -0,0 +1,13 @@
+<?php
+list($s) = strs();
+$n = strlen($s);
+$cnt = $ans = 0;
+for ($i = 0; $i < $n; ++$i) {
+    if ($s[$i] === 'g') $cnt++;
+    else $cnt--;
+}
+echo intdiv($cnt, 2);
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
