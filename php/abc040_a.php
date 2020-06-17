1ffa895efa6cb6c0f76e071b523c0e54b10d8aa0
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-17 18:41:23 +0900

    abc040_abc 自力AC

---
+++ php/abc040_a.php
@@ -0,0 +1,7 @@
+<?php
+list($n, $x) = ints();
+echo min($x - 1, $n - $x);
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
---
+++ php/abc040_b.php
@@ -0,0 +1,13 @@
+<?php
+list($n) = ints();
+$min = PHP_INT_MAX;
+for ($a = (int) sqrt($n); $a >= 1; --$a) {
+    $b = intdiv($n, $a);
+    $c = $n % $a + abs($a - $b);
+    $min = min($min, $c);
+}
+echo $min;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
