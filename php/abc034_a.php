e805952fc3abfb77d0fcec8d5e6d2f9aa82a3d64
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-18 11:12:38 +0900

    abc034_abc 自力AC

---
+++ php/abc034_a.php
@@ -0,0 +1,8 @@
+<?php
+list($x, $y) = ints();
+echo $x < $y ? 'Better' : 'Worse';
+echo PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
---
+++ php/abc034_b.php
@@ -0,0 +1,8 @@
+<?php
+list($n) = ints();
+echo $n % 2 ? $n + 1 : $n - 1;
+echo PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
