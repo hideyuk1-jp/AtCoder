2e8f7f76e4a26ced875575cecc44298b43ad56bd
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-18 21:14:48 +0900

    abc027_ab 自力AC

---
+++ php/abc027_a.php
@@ -0,0 +1,8 @@
+<?php
+$l = ints();
+echo pow(min($l) * max($l), 2) / ($l[0] * $l[1] * $l[2]);
+echo PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
---
+++ php/abc027_b.php
@@ -0,0 +1,22 @@
+<?php
+list($n) = ints();
+$a = ints();
+$sum = array_sum($a);
+if ($sum % $n) exit('-1' . PHP_EOL);
+$avg = intdiv($sum, $n);
+$cp = $ans = 0;
+for ($i = 0; $i < $n; ++$i) {
+    $c = $cp + $a[$i] - $avg;
+    if ($cp === 0 && $c !== 0) $l = $i;
+    if ($cp !== 0 && $c === 0) {
+        $ans += $i - $l;
+        unset($l);
+    }
+    $cp = $c;
+}
+if (isset($l)) $ans += $n - 1 - $l;
+echo $ans . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
