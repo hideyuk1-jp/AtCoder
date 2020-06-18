d5dc415dce3505521eea304364def18c79df95dd
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-18 23:44:33 +0900

    abc020_ab 自力AC

---
+++ php/abc020_a.php
@@ -0,0 +1,8 @@
+<?php
+list($q) = ints();
+echo $q === 1 ? 'ABC' : 'chokudai';
+echo PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
---
+++ php/abc020_b.php
@@ -0,0 +1,8 @@
+<?php
+list($a, $b) = strs();
+echo ($a . $b) * 2;
+echo PHP_EOL;
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
