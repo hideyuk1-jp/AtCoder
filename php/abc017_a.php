355dff01986b2fa4bbcb71cfcf92973f413f7b8f
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-19 00:46:22 +0900

    abc017_ab 自力AC

---
+++ php/abc017_a.php
@@ -0,0 +1,11 @@
+<?php
+$ans = 0;
+for ($i = 0; $i < 3; ++$i) {
+    list($s, $e) = ints();
+    $ans += $s * $e / 10;
+}
+echo $ans . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
---
+++ php/abc017_b.php
@@ -0,0 +1,16 @@
+<?php
+list($x) = strs();
+if ($x === '') exit('YES' . PHP_EOL);
+for ($i = 0; $i < strlen($x); ++$i) {
+    if ($x[$i] === 'o' || $x[$i] === 'k' || $x[$i] === 'u') continue;
+    if ($i < strlen($x) - 1 && $x[$i] === 'c' && $x[$i + 1] === 'h') {
+        ++$i;
+        continue;
+    }
+    exit('NO' . PHP_EOL);
+}
+echo 'YES' . PHP_EOL;
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
