452e80f5e35cc5750b5857ea89f85ecdd24c1f08
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-20 00:49:56 +0900

    arc057_a 自力AC

---
+++ php/arc057_a.php
@@ -0,0 +1,14 @@
+<?php
+list($a, $k) = ints();
+$n = 2 * (10 ** 12);
+if ($k === 0) exit((string) ($n - $a)) . PHP_EOL;
+$i = 0;
+while ($a < $n) {
+    $a += 1 + $a * $k;
+    $i++;
+}
+echo $i . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
