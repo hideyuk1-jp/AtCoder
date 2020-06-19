99a7319d79f67d05aa7fa85c4768625095a5e327
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-19 21:40:38 +0900

    abc008_ab 自力AC

---
+++ php/abc008_b.php
@@ -0,0 +1,17 @@
+<?php
+list($n) = ints();
+for ($i = 0; $i < $n; ++$i) {
+    list($s) = strs();
+    if (isset($cnt[$s])) $cnt[$s]++;
+    else $cnt[$s] = 1;
+}
+$max = max($cnt);
+echo array_flip($cnt)[$max] . PHP_EOL;
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
