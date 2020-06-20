313509bc3407c221a6b1133c979e8e98c69d1d98
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-20 23:52:15 +0900

    agc046_a 自力AC

---
+++ php/agc046_a.php
@@ -0,0 +1,39 @@
+<?php
+list($x) = ints();
+echo intdiv(lcm($x, 360), $x);
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
+// 最大公約数（2つ）
+function gcd($m, $n)
+{
+    if (!$n) return $m;
+    return gcd($n, $m % $n);
+}
+
+// 最大公約数（3つ以上）
+function gcdAll($arr)
+{
+    $gcd = array_pop($arr);
+    foreach ($arr as $a) {
+        $gcd = gcd($gcd, $a);
+    }
+    return $gcd;
+}
+
+// 最小公倍数（2つ）
+function lcm($m, $n)
+{
+    return (int) ($m / gcd($m, $n) * $n); // オーバーフローしないように先に割り算
+}
+
+// 最小公倍数（3つ以上）
+function lcmAll($arr)
+{
+    $lcm = array_pop($arr);
+    foreach ($arr as $a) {
+        $lcm = lcm($lcm, $a);
+    }
+    return $lcm;
+}
