fddc7c17d11f7c9b72b2e315902f5ace649d8151
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-21 16:06:06 +0900

    diverta_2019_2_b 解説見てAC

---
+++ php/diverta_2019_2.php
@@ -0,0 +1,24 @@
+<?php
+list($n) = ints();
+for ($i = 0; $i < $n; ++$i) {
+    list($x[$i], $y[$i]) = ints();
+    $v[$x[$i]][$y[$i]] = true;
+}
+$max = 0;
+for ($i = 0; $i < $n - 1; ++$i) {
+    for ($j = $i + 1; $j < $n; ++$j) {
+        $dx = $x[$j] - $x[$i];
+        $dy = $y[$j] - $y[$i];
+        if (isset($cnt[$dx][$dy])) $cnt[$dx][$dy]++;
+        else $cnt[$dx][$dy] = 1;
+        if (isset($cnt[-$dx][-$dy])) $cnt[-$dx][-$dy]++;
+        else $cnt[-$dx][-$dy] = 1;
+    }
+}
+$max = 0;
+foreach ($cnt as $i => $v) $max = max($max, max($v));
+echo $n - $max;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
