4345e52cb3351fef787c7b1c3805011439d3fc76
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-23 15:53:28 +0900

    abc044_c 解説見て満点AC

--- php/abc044_c.php
+++ php/abc044_c.php
@@ -2,13 +2,17 @@
 list($n, $a) = ints();
 $x = ints();
 for ($i = 0; $i < $n; ++$i) $x[$i] -= $a;
-$cnt = 0;
-for ($i = 0; $i < 2 ** $n; ++$i) {
-    $sum = 0;
-    for ($j = 0; $j < $n; ++$j) if ($i >> $j & 1) $sum += $x[$j];
-    if ($sum === 0) ++$cnt;
+// dp[i][t]=v x0~xiから0枚以上を選んで合計がtになる組み合わせ数v
+$dp[-1][0] = 1;
+for ($i = 0; $i < $n; ++$i) {
+    foreach ($dp[$i - 1] as $t => $v) {
+        if (isset($dp[$i][$t])) $dp[$i][$t] += $v;
+        else $dp[$i][$t] = $v;
+        if (isset($dp[$i][$t + $x[$i]])) $dp[$i][$t + $x[$i]] += $v;
+        else $dp[$i][$t + $x[$i]] = $v;
+    }
 }
-echo $cnt - 1;
+echo $dp[$n - 1][0] - 1; // 1枚も選ばない場合は除去
 function ints()
 {
     return array_map('intval', explode(' ', trim(fgets(STDIN))));
