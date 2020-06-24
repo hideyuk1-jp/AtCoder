94bd3d16f969bfd93a07b719ca9260bc25fdc0af
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-24 19:43:15 +0900

    agc023_a 解説見てAC

---
+++ php/agc023_a.php
@@ -0,0 +1,18 @@
+<?php
+list($n) = ints();
+$a = ints();
+for ($i = 0; $i <= $n; ++$i) $s[$i] = ($s[$i - 1] ?? 0) + ($a[$i - 1] ?? 0);
+for ($i = 0; $i <= $n; ++$i) {
+    if (isset($cnt[$s[$i]])) ++$cnt[$s[$i]];
+    else $cnt[$s[$i]] = 1;
+}
+$ans = 0;
+foreach ($cnt as $i => $v) {
+    if ($i === 0) $v;
+    $ans += $v * ($v - 1) / 2;
+}
+echo $ans . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
