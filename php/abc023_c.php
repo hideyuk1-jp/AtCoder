bbf03d6332601eeb06be5c75907516be65c78405
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-24 13:54:11 +0900

    abc023_c 解説見て満点AC

--- php/abc023_c.php
+++ php/abc023_c.php
@@ -1,25 +1,33 @@
 <?php
 list($R, $C, $K) = ints();
 list($N) = ints();
+$cntr = array_fill(0, $R, 0);
+$cntc = array_fill(0, $C, 0);
 for ($i = 0; $i < $N; ++$i) {
     list($r, $c) = ints();
     --$r;
     --$c;
-    if (isset($cntr[$r])) $cntr[$r]++;
-    else $cntr[$r] = 1;
-    if (isset($cntc[$c])) $cntc[$c]++;
-    else $cntc[$c] = 1;
-    $isExit[$r][$c] = true;
+    $rc[] = [$r, $c];
+    $cntr[$r]++;
+    $cntc[$c]++;
 }
-$ans = 0;
-for ($i = 0; $i < $R; ++$i) {
-    for ($j = 0; $j < $C; ++$j) {
-        $c = ($cntr[$i] ?? 0) + ($cntc[$j] ?? 0);
-        if ($isExit[$i][$j]) --$c;
-        if ($c === $K) ++$ans;
-    }
+foreach ($cntr as $v) {
+    if (isset($cr[$v])) $cr[$v]++;
+    else $cr[$v] = 1;
 }
-echo $ans . PHP_EOL;
+foreach ($cntc as $v) {
+    if (isset($cc[$v])) $cc[$v]++;
+    else $cc[$v] = 1;
+}
+// K個になる数を集計（マスに飴がある場合は重複して数えられる）
+$cnt = 0;
+foreach ($cr as $i => $r) $cnt += $r * ($cc[$K - $i] ?? 0);
+// マスに飴がありK個になる数を引き、マスに飴がありK+1個になる数を足す
+foreach ($rc as list($r, $c)) {
+    if ($cntr[$r] + $cntc[$c] === $K) --$cnt;
+    if ($cntr[$r] + $cntc[$c] === $K + 1) ++$cnt;
+}
+echo $cnt . PHP_EOL;
 function ints()
 {
     return array_map('intval', explode(' ', trim(fgets(STDIN))));
