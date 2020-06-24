0014b86ccacf06c31c98b3d4e0afe88aad168dd6
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-24 12:00:02 +0900

    abc013_c 自力AC

---
+++ php/abc013_c.php
@@ -0,0 +1,23 @@
+<?php
+list($n, $h) = ints();
+list($a, $b, $c, $d, $e) = ints();
+$cost = PHP_INT_MAX;
+for ($i = 0; $i <= $n; ++$i) {
+    if ($h + $i * $b > ($n - $i) * $e) {
+        $cost = min($cost, $i * $a);
+        break;
+    }
+    $ok = $n - $i + 1;
+    $ng = -1;
+    while (abs($ok - $ng) > 1) {
+        $mid = intdiv($ok + $ng, 2);
+        if ($h + $i * $b + $mid * $d > ($n - $i - $mid) * $e) $ok = $mid;
+        else $ng = $mid;
+    }
+    if ($ok !== $n - $i + 1) $cost = min($cost, $i * $a + $ok * $c);
+}
+echo $cost . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
