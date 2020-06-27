54526137c3344ec937db44f96d6567cf19fe7644
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-28 00:11:20 +0900

    arc028_a 自力AC

---
+++ php/arc028_a.php
@@ -0,0 +1,8 @@
+<?php
+list($n, $a, $b) = ints();
+echo ($n - 1) % ($a + $b) < $a ? 'Ant' : 'Bug';
+echo PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
