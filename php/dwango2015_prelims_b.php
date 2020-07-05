86c1e8ece907a0f787c0537bf412869e56d18aac
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-06 01:56:07 +0900

    ac green diff x 2

---
+++ php/dwango2015_prelims_b.php
@@ -0,0 +1,23 @@
+<?php
+list($s) = strs();
+$n = strlen($s);
+$k = 0;
+$a[$k] = 0;
+for ($i = 0; $i < $n - 1; ++$i) {
+    if ($s[$i] === '2' && $s[$i + 1] === '5') {
+        ++$a[$k];
+        ++$i;
+        continue;
+    }
+    if ($a[$k] > 0) {
+        ++$k;
+        $a[$k] = 0;
+    }
+}
+$ans = 0;
+foreach ($a as $v) $ans += (1 + $v) * $v / 2;
+echo $ans . PHP_EOL;
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
--- php/tenka1_2018_c.php
+++ php/tenka1_2018_c.php
@@ -1,8 +1,27 @@
 <?php
 list($n) = ints();
-for ($i = 0; $i < $n; ++$i) {
-    list($a[]) = ints();
+for ($i = 0; $i < $n; ++$i) list($a[]) = ints();
+$_a = $a;
+rsort($a);
+sort($_a);
+for ($i = 0; $i < intdiv($n, 2); ++$i) {
+    if ($i % 2 === 0) {
+        $r[] = $_a[$i];
+        $l[] = $a[$i];
+    } else {
+        $r[] = $a[$i];
+        $l[] = $_a[$i];
+    }
 }
+if ($n % 2) {
+    if (abs($r[$i - 1] - $a[$i]) > abs($l[$i - 1] - $a[$i])) $r[] = $a[$i];
+    else $l[] = $a[$i];
+}
+$aa = array_merge(array_reverse($l), $r);
+$ans = 0;
+for ($i = 0; $i < $n - 1; ++$i)
+    $ans += abs($aa[$i + 1] - $aa[$i]);
+echo $ans;
 function ints()
 {
     return array_map('intval', explode(' ', trim(fgets(STDIN))));
