06cfc4d51761386084e1a0000edba6ce9df2fc1c
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-08 10:16:21 +0900

    ac cyan diff x 3

---
+++ php/arc052_b.php
@@ -0,0 +1,28 @@
+<?php
+list($n, $q) = ints();
+for ($i = 0; $i < $n; ++$i) $c[] = ints();
+for ($i = 0; $i < $q; ++$i) {
+    list($a, $b) = ints();
+    $ans[$i] = 0;
+    for ($k = 0; $k < $n; ++$k) {
+        list($x, $r, $h) = $c[$k];
+        if ($x >= $b || $x + $h <= $a) continue;
+        if ($x >= $a) {
+            $ans[$i] += $r * $r * pi() * $h / 3;
+        } else {
+            $_h = ($x + $h) - $a;
+            $_r = $r * $_h / $h;
+            $ans[$i] += $_r * $_r * pi() * $_h / 3;
+        }
+        if ($x + $h > $b) {
+            $_h = ($x + $h) - $b;
+            $_r = $r * $_h / $h;
+            $ans[$i] -= $_r * $_r * pi() * $_h / 3;
+        }
+    }
+}
+echo implode(PHP_EOL, $ans) . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
---
+++ php/tenka1_2015_qualB_b.php
@@ -0,0 +1,15 @@
+<?php
+list($s) = strs();
+if ($s === '{}') exit('dict' . PHP_EOL);
+$d === 0;
+for ($i = 0; $i < strlen($s); ++$i) {
+    if ($s[$i] === '{') ++$d;
+    elseif ($s[$i] === '}') --$d;
+    elseif ($s[$i] === ':' && $d === 1) exit('dict' . PHP_EOL);
+    elseif ($s[$i] === ',' && $d === 1) exit('set' . PHP_EOL);
+}
+echo 'set' . PHP_EOL;
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
