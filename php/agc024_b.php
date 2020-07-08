7bc351a3ed8be0e8b9e08ccdf9948e75ee15eb39
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-08 14:17:42 +0900

    解説ac agc024_b(cyan diff)

---
+++ php/agc024_b.php
@@ -0,0 +1,12 @@
+<?php
+list($n) = ints();
+$dp = array_fill(0, $n + 1, 0);
+for ($i = 0; $i < $n; ++$i) {
+    list($p) = ints();
+    $dp[$p] = $dp[$p - 1] + 1;
+}
+echo $n - max($dp);
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
