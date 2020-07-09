349642a44f121c6300b316ac6543df0578923b7d
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-09 11:10:32 +0900

    自力ac tenka1_2013_qualB_b(cyan diff)

---
+++ php/tenka1_2013_qualB_b.php
@@ -0,0 +1,94 @@
+<?php
+list($q, $l) = ints();
+$s = new MyStack($l);
+$ans = [];
+for ($i = 0; $i < $q; ++$i) {
+    list($type, $n, $v) = strs();
+    $n = (int) $n;
+    if (isset($e)) continue;
+    if ($type === 'Push') {
+        if (!$s->push($v, $n)) {
+            $ans[] = 'FULL';
+            $e = true;
+        }
+    } elseif ($type === 'Pop') {
+        if (!$s->pop($n)) {
+            $ans[] = 'EMPTY';
+            $e = true;
+        }
+    } elseif ($type === 'Top') {
+        $top = $s->top();
+        if ($top !== false) {
+            $ans[] = $top;
+        } else {
+            $ans[] = 'EMPTY';
+            $e = true;
+        }
+    } elseif ($type === 'Size') {
+        $ans[] = $s->size();
+    }
+}
+if (!isset($e)) $ans[] = 'SAFE';
+echo implode(PHP_EOL, $ans);
+class MyStack
+{
+    private $l;
+    private $a;
+    private $size;
+
+    function __construct($l)
+    {
+        $this->l = $l;
+        $this->a = [];
+        $this->size = 0;
+    }
+
+    function push($v, $n)
+    {
+        if ($this->l < $this->size + $n) return false;
+        $this->a[] = [$v, $n];
+        $this->size += $n;
+        return true;
+    }
+
+    function pop($n)
+    {
+        $a = $this->a;
+        $size = $this->size;
+        if ($size < $n) return false;
+        if (count($a) === 0) return false;
+        list($v, $m) = array_pop($a);
+        $size -= $m;
+        while ($m < $n) {
+            $n -= $m;
+            if (count($a) === 0) return false;
+            list($v, $m) = array_pop($a);
+            $size -= $m;
+        }
+        if ($size < 0) return false;
+        $this->a = $a;
+        $this->size = $size;
+        if ($m > $n) $this->push($v, $m - $n);
+        return true;
+    }
+
+    function top()
+    {
+        $a = $this->a;
+        if (count($a) === 0) return false;
+        return $a[count($a) - 1][0];
+    }
+
+    function size()
+    {
+        return $this->size;
+    }
+}
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
