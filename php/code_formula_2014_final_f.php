d173dc6e33dd14a6478f46b8269189e74f765211
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-12 11:22:51 +0900

    解説ac cyan diff x 1

---
+++ php/code_formula_2014_final_f.php
@@ -0,0 +1,16 @@
+<?php
+$x = 0;
+$y = 100;
+$prowr = 100;
+for ($i = 100; $i >= 1; --$i) {
+    if ($x + $i * 2 > 1500) {
+        // 1段下へ
+        $x = 0;
+        $y += $prowr + $i;
+        $prowr = $i;
+    }
+    $x += $i;
+    $ans[] = "${x} ${y}";
+    $x += $i;
+}
+echo implode(PHP_EOL, array_reverse($ans));
