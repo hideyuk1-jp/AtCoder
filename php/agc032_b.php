9fbf72d64da1fe9d83b241e21eff03d0c54d6315
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-14 11:56:41 +0900

    解説ac cyan diff x 1

---
+++ php/agc032_b.php
@@ -0,0 +1,16 @@
+<?php
+list($n) = ints();
+for ($i = 1; $i <= intdiv($n, 2); ++$i) {
+    if ($n % 2) $g[$i][$n - $i] = false;
+    else $g[$i][$n - $i + 1] = false;
+}
+echo (intdiv($n * ($n - 1), 2) - intdiv($n, 2)) . PHP_EOL;
+for ($i = 1; $i <= $n - 1; ++$i) {
+    for ($j = $i + 1; $j <= $n; ++$j) {
+        if (!isset($g[$i][$j])) echo $i . ' ' . $j . PHP_EOL;
+    }
+}
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
