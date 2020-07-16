e9c9f456414bd8ff31132858b36e1602f2c52dca
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-16 12:01:16 +0900

    自力ac green diff x 1

---
+++ php/abc134_d.php
@@ -0,0 +1,15 @@
+<?php
+list($n) = ints();
+$a = ints();
+for ($i = $n - 1; $i >= 0; --$i) {
+    $t = 0;
+    for ($j = $i + $i + 1; $j < $n; $j += $i + 1) $t += $b[$j];
+    $b[$i] = ($t + $a[$i]) % 2;
+}
+for ($i = 0; $i < $n; ++$i) if ($b[$i] === 1) $ans[] = $i + 1;
+echo count($ans) . PHP_EOL;
+echo implode(' ', $ans);
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
