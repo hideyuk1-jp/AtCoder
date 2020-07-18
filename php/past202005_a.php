507af197585f7b179ba24a4d21f16d38dd70ddd6
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-18 21:33:53 +0900

    1 ac

---
+++ php/past202005_a.php
@@ -0,0 +1,10 @@
+<?php
+list($s) = strs();
+list($t) = strs();
+if ($s === $t) echo 'same';
+elseif (strtolower($s) === strtolower($t)) echo 'case-insensitive';
+else echo 'different';
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
