975c2b9b23ff9f7d4e3792c862b25ce08bd02e1c
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-22 15:35:36 +0900

    自力ac past202004 x 3

--- php/past202004_g.php
+++ php/past202004_g.php
@@ -1,12 +1,30 @@
 <?php
 list($q) = ints();
-$ms = new MyStr();
+$strs = [];
+$top = 0;
 for ($i = 0; $i < $q; ++$i) {
     list($type, $a, $b) = strs();
     if ($type === '1') {
-        $ms->insert($a, (int)$b);
+        $strs[] = [$a, (int)$b];
     } elseif ($type === '2') {
-        $ans[] = $ms->delete((int)$a);
+        $x = (int)$a;
+        $cnt = [];
+        for ($j = 0; $j < 26; ++$j) $cnt[chr(97 + $j)] = 0;
+        while ($x > 0 && $top < count($strs)) {
+            list($c, $n) = $strs[$top];
+            if ($x >= $n) {
+                $cnt[$c] += $n;
+                ++$top;
+                $x -= $n;
+            } else {
+                $cnt[$c] += $x;
+                $strs[$top][1] -= $x;
+                break;
+            }
+        }
+        $res = 0;
+        foreach ($cnt as $v) $res += $v ** 2;
+        $ans[] = $res;
     }
 }
 echo implode(PHP_EOL, $ans);
@@ -17,48 +35,4 @@ function strs()
 function ints()
 {
     return array_map('intval', explode(' ', trim(fgets(STDIN))));
-}
-class MyStr
-{
-    private $strs;
-    private $top;
-
-    function __construct()
-    {
-        $this->strs = [];
-        $this->top = 0;
-    }
-
-    function insert($c, $x)
-    {
-        $this->strs[] = [$c, $x];
-    }
-
-    function delete($x)
-    {
-        $cnt = [];
-        $strs = $this->strs;
-        $top = $this->top;
-        while ($x > 0) {
-            if (!isset($strs[$top])) break;
-            list($c, $n) = $strs[$top];
-            if ($x >= $n) {
-                if (isset($cnt[$c])) $cnt[$c] += $n;
-                else $cnt[$c] = $n;
-                ++$top;
-                $x -= $n;
-            } else {
-                if (isset($cnt[$c])) $cnt[$c] += $x;
-                else $cnt[$c] = $x;
-                $strs[$top][1] = $n - $x;
-                break;
-            }
-        }
-        $this->strs = $strs;
-        $this->top = $top;
-        $res = 0;
-        foreach ($cnt as $c)
-            $res += $c ** 2;
-        return $res;
-    }
 }
---
+++ php/past202004_i.php
@@ -0,0 +1,21 @@
+<?php
+list($n) = ints();
+$a = ints();
+$_a = array_flip($a);
+$ans = array_fill(0, 2 ** $n, 0);
+$c = 2 ** $n;
+while ($c > 1) {
+    for ($i = 0; $i < $c; $i += 2) {
+        $p1 = $a[$i];
+        $p2 = $a[$i + 1];
+        ++$ans[$_a[$p1]];
+        ++$ans[$_a[$p2]];
+        $a[intdiv($i, 2)] = max($p1, $p2);
+    }
+    $c = intdiv($c, 2);
+}
+echo implode(PHP_EOL, $ans);
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
