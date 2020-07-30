5c406c77d478490be0679e0e1aba1779405dac96
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-30 16:46:03 +0900

    ac cyan diff x 1

---
+++ php/lib/151_bitDP.php
@@ -0,0 +1,27 @@
+<?php
+// bitDP abc142_e
+list($n, $m) = ints();
+for ($i = 0; $i < $m; ++$i) {
+    list($a[], $b) = ints();
+    $c = ints();
+    // どの宝箱を開けられるか bit で管理
+    $d = 0;
+    for ($j = 0; $j < $b; ++$j) {
+        --$c[$j];
+        $d |= 1 << $c[$j];
+    }
+    $bits[] = $d;
+}
+$dp = array_fill(0, $m + 1, array_fill(0, 1 << $n, INF));
+$dp[0][0] = 0;
+for ($i = 0; $i < $m; ++$i) {
+    for ($j = 0; $j < (1 << $n); ++$j) {
+        $dp[$i + 1][$j] = min($dp[$i + 1][$j], $dp[$i][$j]);
+        $dp[$i + 1][$j | $bits[$i]] = min($dp[$i + 1][$j | $bits[$i]], $dp[$i][$j] + $a[$i]);
+    }
+}
+echo $dp[$m][(1 << $n) - 1] !== INF ? $dp[$m][(1 << $n) - 1] : -1;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
---
+++ php/lib/152_digitDP.php
@@ -0,0 +1,29 @@
+<?php
+// digitDP
+// N以下の整数で、ある条件を満たすものの個数を求める場合や
+// N以下の整数で、ある条件を満たすものの最大値を求める場合などに使える
+
+// abc007_d
+// A以上B以下の整数でいずれかの桁に4か9を含むものの個数を求める問題
+list($a, $b) = strs();
+echo (countHateNum($b) - countHateNum($a - 1)) . PHP_EOL;
+function countHateNum(String $s): int
+{
+    $l = strlen($s);
+    $dp[0][0][0] = 1;
+    for ($i = 1; $i <= $l; ++$i) {
+        $n = (int)$s[$i - 1];
+        foreach ([0, 1] as $smaller) {
+            foreach ([0, 1] as $hate_flag) {
+                for ($x = 0; $x <= ($smaller ? 9 : $n); ++$x) {
+                    $dp[$i][$smaller || $x < $n][$hate_flag || $x === 4 || $x === 9] += ($dp[$i - 1][$smaller][$hate_flag] ?? 0);
+                }
+            }
+        }
+    }
+    return $dp[$l][0][1] + $dp[$l][1][1];
+}
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
