b50a672df68bb29f353cbe570e5f60d02e8e9abc
hideyuk1-jp <hh11028+github@gmail.com>
2020-08-04 22:04:57 +0900

    arc098_b 解説ac

---
+++ php/arc098_b.php
@@ -0,0 +1,21 @@
+<?php
+list($n) = ints();
+$a = ints();
+// 尺取法
+$ans = 0;
+$r = 0;
+$x = 0;
+for ($l = 0; $l < $n; ++$l) {
+    $r = max($r, $l);
+    while ($r < $n && ($x ^ $a[$r]) === ($x + $a[$r])) {
+        $x += $a[$r];
+        ++$r;
+    }
+    $ans += $r - $l;
+    $x -= $a[$l];
+}
+echo $ans;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
