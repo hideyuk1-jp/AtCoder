74b985ef7a356969a09748b1c295e05203b5b5ba
hideyuk1-jp <hh11028+github@gmail.com>
2020-08-07 09:08:54 +0900

    abc026_d 解説ac

---
+++ php/abc026_d.php
@@ -0,0 +1,16 @@
+<?php
+// 二分探索
+list($a, $b, $c) = ints();
+$digit = 10 ** 12;
+$r = 200 * $digit;
+$l = 0 * $digit;
+while (abs($r - $l) > 1) {
+    $mid = intdiv($r + $l, 2);
+    if ($a * $mid / $digit + $b * sin($c * $mid / $digit * pi()) >= 100) $r = $mid;
+    else $l = $mid;
+}
+echo ($r / $digit);
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
