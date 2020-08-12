ee2b5d85d132a41bf7adc57124d535efdb17f90b
hideyuk1-jp <hh11028+github@gmail.com>
2020-08-12 14:50:31 +0900

    abc002_d 解説ac

---
+++ php/abc002_d.php
@@ -0,0 +1,25 @@
+<?php
+list($n, $m) = ints();
+for ($i = 0; $i < $m; ++$i) {
+    list($x, $y) = ints();
+    --$x;
+    --$y;
+    $g[$x][$y] = true;
+    $g[$y][$x] = true;
+}
+$max = 1;
+for ($i = 0; $i < (1 << $n); ++$i) {
+    $member = [];
+    for ($j = 0; $j < $n; ++$j)
+        if ($i >> $j & 1) $member[] = $j;
+    $l = count($member);
+    for ($j = 0; $j < $l - 1; ++$j)
+        for ($k = $j + 1; $k < $l; ++$k)
+            if (!isset($g[$member[$j]][$member[$k]])) continue 3;
+    $max = max($max, $l);
+}
+echo $max, PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
