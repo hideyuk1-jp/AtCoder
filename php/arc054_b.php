b8564f595f1b17ec46475b2af0f9fd0938cc964d
hideyuk1-jp <hh11028+github@gmail.com>
2020-08-13 14:54:53 +0900

    arc054_b 解説ac

---
+++ php/arc054_b.php
@@ -0,0 +1,17 @@
+<?php
+$p = (float)trim(fgets(STDIN));
+// 三分探索
+$l = 0;
+$r = 100;
+while (abs($r - $l) > 10 ** (-5)) {
+    $ld = $l + abs($r - $l) / 3;
+    $rd = $r - abs($r - $l) / 3;
+    if (f($ld) > f($rd)) $l = $ld;
+    else $r = $rd;
+}
+echo f($l), PHP_EOL;
+function f($x)
+{
+    global $p;
+    return $x + $p * (2 ** (-$x / 1.5));
+}
---
+++ php/lib/71_三分探索.php
@@ -0,0 +1,17 @@
+<?php
+// 三分探索　極大値や極小値を見つける
+$p = (float)trim(fgets(STDIN));
+$l = 0;
+$r = 100;
+while (abs($r - $l) > 10 ** (-5)) {
+    $ld = $l + abs($r - $l) / 3;
+    $rd = $r - abs($r - $l) / 3;
+    if (f($ld) > f($rd)) $l = $ld;
+    else $r = $rd;
+}
+echo f($l), PHP_EOL;
+function f($x)
+{
+    global $p;
+    return $x + $p * (2 ** (-$x / 1.5));
+}
