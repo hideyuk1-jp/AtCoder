10a98b34a85190c9b76bf9bda75eec5c65645a47
hideyuk1-jp <hh11028+github@gmail.com>
2020-08-18 12:36:47 +0900

    arc046_b 解説ac(cyan)

---
+++ php/arc046_b.php
@@ -0,0 +1,16 @@
+<?php
+list($n) = ints();
+list($a, $b) = ints();
+if ($a === $b) {
+    if ($n <= $a) $ans = 'Takahashi';
+    elseif ($n % ($a + 1) === 0) $ans = 'Aoki';
+    else $ans = 'Takahashi';
+} else {
+    if ($a > $b || $n <= $a) $ans = 'Takahashi';
+    else $ans = 'Aoki';
+}
+echo $ans, PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
