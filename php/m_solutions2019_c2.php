e8a771acbef75000cd22d67aed9947c1e7ad098b
hideyuk1-jp <hh11028+github@gmail.com>
2020-08-24 14:48:23 +0900

    m_solutions2019_c 高速化

---
+++ php/m_solutions2019_c2.php
@@ -0,0 +1,129 @@
+<?php
+const MOD = 10 ** 9 + 7;
+list($n, $a, $b, $c) = ints();
+$a = modDiv($a, 100 - $c);
+$b = modDiv($b, 100 - $c);
+init($n * 2);
+// まず引き分けがないものとして期待値を計算
+$sum = 0;
+$powan = modPow($a, $n);
+$powai = 1;
+$powbn = modPow($b, $n);
+$powbi = 1;
+for ($i = 0; $i < $n; ++$i) {
+    // Aがn回勝つ、Bはi回勝つ
+    $pa = modProd(
+        $powan,
+        $powbi
+    );
+    // Bがn回勝つ、Aはi回勝つ
+    $pb = modProd(
+        $powbn,
+        $powai
+    );
+    $sum = modAdd($sum, modProd($n + $i, nCr2($n - 1 + $i, $n - 1), modAdd($pa, $pb)));
+    $powai = modMul($powai, $a);
+    $powbi = modMul($powbi, $b);
+}
+// 引き分けを含んだ期待値を計算
+$sum = modMul($sum, modDiv(100, 100 - $c));
+echo $sum;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
+// 足し算
+function modAdd($x, $y)
+{
+    return ($x + $y) % MOD;
+}
+
+// SUM
+function modSum(...$xs)
+{
+    $res = 0;
+    foreach ($xs as $x) $res = modAdd($res, $x);
+    return $res;
+}
+
+// 引き算
+function modSub($x, $y)
+{
+    return ($x + MOD - $y) % MOD;
+}
+
+// 掛け算
+function modMul($x, $y)
+{
+    return ($x * $y) % MOD;
+}
+
+// PRODUCT
+function modProd(...$xs)
+{
+    $res = 1;
+    foreach ($xs as $x) $res = modMul($res, $x);
+    return $res;
+}
+
+// 割り算
+function modDiv($x, $y)
+{
+    return modMul($x, modPow($y, MOD - 2));
+}
+
+// 累乗（繰り返し二乗法）
+function modPow($n, $x)
+{
+    if ($x === 0) return 1;
+    $res = (modPow($n, $x >> 1) ** 2) % MOD;
+    if ($x % 2 === 1) $res = modMul($res, $n);
+    return $res;
+}
+
+// 階乗
+function modFac($n)
+{
+    if ($n === 0) return 1;
+    return modMul($n, modFac($n - 1));
+}
+
+// 順列
+function nPr($n, $r)
+{
+    if ($r === 0) return 1;
+    return modMul(nPr($n, $r - 1), $n - $r + 1);
+}
+
+// 組み合わせ
+function nCr($n, $r)
+{
+    $r = min($r, $n - $r);
+    if ($r === 0) return 1;
+    return modDiv(nPr($n, $r), modFac($r));
+}
+
+// 階乗、逆元を前処理
+function init($n)
+{
+    global $fact, $ifact;
+    $fact[0] = 1;
+    for ($i = 1; $i <= $n; ++$i) $fact[$i] = modMul($fact[$i - 1], $i);
+    $ifact[$n] = modDiv(1, $fact[$n]);
+    for ($i = $n; $i >= 1; --$i)
+        $ifact[$i - 1] = modMul($ifact[$i], $i);
+}
+
+// 順列（前処理）
+function nPr2($n, $r)
+{
+    global $fact, $ifact;
+    return modMul($fact[$n], $ifact[$n - $r]);
+}
+
+// 組み合わせ（前処理）
+function nCr2($n, $r)
+{
+    global $fact, $ifact;
+    return modMul(modMul($fact[$n], $ifact[$r]), $ifact[$n - $r]);
+}
