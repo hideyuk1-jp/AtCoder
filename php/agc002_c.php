687b79e4255b2314f2df6279a34bded1eae4e748
hideyuk1-jp <hh11028+github@gmail.com>
2020-09-07 07:39:20 +0900

    agc002_c 自力ac(diff:1564)

---
+++ php/agc002_c.php
@@ -0,0 +1,16 @@
+<?php
+list($n, $l) = ints();
+$a = ints();
+for ($i = 0; $i < $n - 1; ++$i) {
+    if ($a[$i] + $a[$i + 1] < $l) continue;
+
+    echo 'Possible', PHP_EOL;
+    for ($j = 1; $j <= $i; ++$j) echo $j, PHP_EOL;
+    for ($j = $n - 1; $j > $i; --$j) echo $j, PHP_EOL;
+    exit;
+}
+echo 'Impossible', PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
