fccbb3f42cc98f71bd5a06b740173cfd7d6e96ca
hideyuk1-jp <hh11028+github@gmail.com>
2021-01-31 10:54:53 +0900

    abc180_c

---
+++ php/abc180_c.php
@@ -0,0 +1,21 @@
+<?php
+[$N] = ints();
+echo implode(PHP_EOL, divisors($N));
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
+// 約数列挙
+function divisors($max)
+{
+    $arr = [];
+    $rmax = (int) floor(sqrt($max));
+    for ($i = 1; $i <= $rmax; $i++) {
+        if ($max % $i === 0) {
+            $arr[] = $i;
+            $arr[] = $max / $i;
+        }
+    }
+    sort($arr);
+    return array_unique($arr);
+}
