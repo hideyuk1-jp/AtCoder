813d2c7ba925fcdbf8acb90fd6803e71f939d1a2
hideyuk1-jp <hh11028+github@gmail.com>
2021-02-01 11:43:51 +0900

    abc179_b

---
+++ php/abc179_b.php
@@ -0,0 +1,15 @@
+<?php
+[$N] = ints();
+$cnt = 0;
+$flag = false;
+for ($i = 0; $i < $N; ++$i) {
+    [$d1, $d2] = ints();
+    if ($d1 === $d2) ++$cnt;
+    else $cnt = 0;
+    if ($cnt === 3) $flag = true;
+}
+echo $flag ? 'Yes' . PHP_EOL : 'No' . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
