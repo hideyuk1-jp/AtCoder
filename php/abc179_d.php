c4756bee1493ae38a6d8d9299b744fc4215cd19e
hideyuk1-jp <hh11028+github@gmail.com>
2021-02-06 13:16:25 +0900

    abc179_d imos法っぽくやって自力で通せたけど汚い

---
+++ php/abc179_d.php
@@ -0,0 +1,31 @@
+<?php
+define("MOD", 998244353);
+[$N, $K] = ints();
+for ($i = 0; $i < $K; ++$i) {
+    [$L[], $R[]] = ints();
+}
+array_multisort($L, SORT_ASC, $R);
+$s = array_fill(1, $N, 0);
+$s[1] = 1;
+$s[2] = -1;
+$cnt = 0;
+for ($d = 1; $d <= $N; ++$d) {
+    $cnt += $s[$d] + MOD;
+    $cnt %= MOD;
+    if ($cnt < 1) continue;
+
+    for ($i = 0; $i < $K; ++$i) {
+        if ($d + $L[$i] > $N) break;
+        $s[$d + $L[$i]] += $cnt;
+        $s[$d + $L[$i]] %= MOD;
+        if ($d + $R[$i] + 1 <= $N) {
+            $s[$d + $R[$i] + 1] -= $cnt;
+            $s[$d + $R[$i] + 1] %= MOD;
+        }
+    }
+}
+echo $cnt . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
