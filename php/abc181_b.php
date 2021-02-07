b8a6ae89f6703401e0cb3910ccddb8287ba652b9
hideyuk1-jp <hh11028+github@gmail.com>
2021-02-07 09:40:20 +0900

    abc181_b

---
+++ php/abc181_b.php
@@ -0,0 +1,12 @@
+<?php
+[$N] = ints();
+$cnt = 0;
+for ($i = 0; $i < $N; ++$i) {
+    [$A, $B] = ints();
+    $cnt += ($B - $A + 1) * ($A + $B) / 2;
+}
+echo $cnt . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
