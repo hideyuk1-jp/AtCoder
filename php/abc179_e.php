f9762fe841c2f53ce0625644c1f3a0581369af09
hideyuk1-jp <hh11028+github@gmail.com>
2021-02-11 20:54:25 +0900

    abc179_e

---
+++ php/abc179_e.php
@@ -0,0 +1,28 @@
+<?php
+const DEBUG = false;
+[$N, $X, $M] = ints();
+$a = $X;
+$ans = 0;
+for ($i = 0; $i < $N; ++$i) {
+    if (DEBUG) echo "i=${i}: ${a} ${ans}" . PHP_EOL;
+    if (isset($exits[$a])) {
+        // ループ区間発見
+        if (DEBUG) echo '[loop is find]' . PHP_EOL;
+        $li = $i - $exits[$a][0];
+        $ld = $ans - $exits[$a][1];
+        $ln = intdiv($N - 1 - $i, $li);
+        $ans += $ld * $ln;
+        $i += $li * $ln - 1;
+        $exits = [];
+        continue;
+    } else {
+        $exits[$a] = [$i, $ans];
+    }
+    $ans += $a;
+    $a = ($a ** 2) % $M;
+}
+echo $ans . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
