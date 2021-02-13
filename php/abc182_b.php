e46a1f6a8e844096a587ea76b136c483d0901fc9
hideyuk1-jp <hh11028+github@gmail.com>
2021-02-13 14:04:43 +0900

    abc182_b

---
+++ php/abc182_b.php
@@ -0,0 +1,21 @@
+<?php
+[$N] = ints();
+$a = ints();
+$max_a = max($a);
+$max_cnt = 0;
+$ans = $max_a;
+for ($k = 2; $k <= $max_a; ++$k) {
+    $cnt = 0;
+    for ($i = 0; $i < $N; ++$i) {
+        if ($a[$i] % $k === 0) ++$cnt;
+    }
+    if ($cnt > $max_cnt) {
+        $max_cnt = $cnt;
+        $ans = $k;
+    }
+}
+echo $ans . PHP_EOL;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
