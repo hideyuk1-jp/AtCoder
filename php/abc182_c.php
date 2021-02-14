30914c5ed1f54dba0995c8847dc38d8a821fd401
hideyuk1-jp <hh11028+github@gmail.com>
2021-02-14 13:22:40 +0900

    abc182_c

---
+++ php/abc182_c.php
@@ -0,0 +1,22 @@
+<?php
+[$S] = strs();
+$n = strlen($S);
+$min = $n;
+// bit全探索
+for ($i = 0; $i < 2 ** $n; $i++) {
+    $s = '';
+    for ($j = 0; $j < $n; $j++) {
+        if (($i >> $j) & 1) {
+            $s .= $S[$j];
+        }
+    }
+    if ((int)$s % 3 === 0) {
+        $min = min($min, $n - strlen($s));
+    }
+}
+echo $min < $n ? $min : '-1';
+echo PHP_EOL;
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
