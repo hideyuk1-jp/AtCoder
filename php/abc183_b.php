da851226096c802e07e7d176a874b17c8e1d7ecf
hideyuk1-jp <hh11028+github@gmail.com>
2021-02-17 11:33:51 +0900

    abc183_b

---
+++ php/abc183_b.php
@@ -0,0 +1,10 @@
+<?php
+[$x1, $y1, $x2, $y2] = ints();
+// -y1 = ax1 + b, y2 = ax2 + b
+$a = ($y2 + $y1) / ($x2 - $x1);
+$b = $y2 - $a * $x2;
+echo -$b / $a;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
