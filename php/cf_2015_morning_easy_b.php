97cb3a22934bd971fdc722c1395705d465897ba0
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-06 07:23:32 +0900

    朝ac

---
+++ php/cf_2015_morning_easy_b.php
@@ -0,0 +1,17 @@
+<?php
+[$N] = ints();
+[$S] = strs();
+if ($N % 2) exit('-1' . PHP_EOL);
+$cnt = 0;
+for ($i = 0; $i < $N / 2; ++$i) {
+    if ($S[$i] !== $S[$N / 2 + $i]) ++$cnt;
+}
+echo $cnt, PHP_EOL;
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
