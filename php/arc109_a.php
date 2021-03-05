831a699e39758c73e1500cbe3d4c8957f684994e
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-05 18:59:14 +0900

    帰りの電車でAC

---
+++ php/arc109_a.php
@@ -0,0 +1,10 @@
+<?php
+[$a, $b, $x, $y] = ints();
+if ($a > $b)
+    echo $x + (abs($b - $a) - 1) * min($y, 2 * $x);
+else
+    echo $x + abs($b - $a) * min($y, 2 * $x);
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
