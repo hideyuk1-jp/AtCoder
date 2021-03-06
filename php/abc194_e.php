203090f579fb7151169372b1df7ae661f1da9c38
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-07 02:01:22 +0900

    ちょいリファクタリング

--- php/abc194_e.php
+++ php/abc194_e.php
@@ -5,6 +5,7 @@ $a = ints();
 // prev[num]: 前に数字numが出現したindexを記録
 $prev = array_fill(0, $N + 1, -1);
 // intervalMax[num]: 数字numが現れない最も長い区間を記録
+// これがM以上だとmex minの答えの候補
 $intervalMax = array_fill(0, $N + 1, 0);
 for ($i = 0; $i < $N; ++$i) {
     $intervalMax[$a[$i]] = max($intervalMax[$a[$i]], $i - $prev[$a[$i]] - 1);
