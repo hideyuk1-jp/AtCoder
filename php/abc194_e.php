90b75937975604a1dd78ef14e92cb26ff119ea7b
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-07 02:00:03 +0900

    ちょいリファクタリング

--- php/abc194_e.php
+++ php/abc194_e.php
@@ -1,17 +1,20 @@
 <?php
+// mex
 [$N, $M] = ints();
 $a = ints();
-$x = array_fill(0, $N + 1, -1);
-$max = array_fill(0, $N + 1, 0);
+// prev[num]: 前に数字numが出現したindexを記録
+$prev = array_fill(0, $N + 1, -1);
+// intervalMax[num]: 数字numが現れない最も長い区間を記録
+$intervalMax = array_fill(0, $N + 1, 0);
 for ($i = 0; $i < $N; ++$i) {
-    $max[$a[$i]] = max($max[$a[$i]], $i - $x[$a[$i]]);
-    $x[$a[$i]] = $i;
+    $intervalMax[$a[$i]] = max($intervalMax[$a[$i]], $i - $prev[$a[$i]] - 1);
+    $prev[$a[$i]] = $i;
 }
 for ($i = 0; $i <= $N; ++$i) {
-    $max[$i] = max($max[$i], $N - $x[$i]);
+    $intervalMax[$i] = max($intervalMax[$i], $N - $prev[$i] - 1);
 }
 for ($i = 0; $i <= $N; ++$i) {
-    if ($max[$i] > $M) {
+    if ($intervalMax[$i] >= $M) {
         echo $i, PHP_EOL;
         break;
     }
