bf8ba0e57a8481e57f3a8c7b4c704a8488bcbd36
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-08 00:20:29 +0900

    茶diffやけどむずい

---
+++ php/arc107_b.php
@@ -0,0 +1,14 @@
+<?php
+[$N, $K] = ints();
+for ($i = 2; $i <= 2 * $N; $i++) {
+    $x[$i] = min($i - 1, 2 * $N + 1 - $i);
+}
+$ans = 0;
+for ($i = 2; $i <= 2 * $N; $i++) {
+    $ans += $x[$i] * ($x[$i - $K] ?? 0);
+}
+echo $ans;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
