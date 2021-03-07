05e93927f3858ae9946fc3c96d475dd06534ff61
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-07 10:44:28 +0900

    abc112_c 朝ac。時間かけすぎた。15分くらいかかった

---
+++ php/arc112_a.php
@@ -0,0 +1,16 @@
+<?php
+[$N] = ints();
+for ($i = 0; $i < $N; ++$i) {
+    [$l, $r] = ints();
+    if ($r - $l < $l) {
+        $ans[] = 0;
+        continue;
+    }
+    $max = $r - $l + 1 - $l;
+    $ans[] = (1 + $max) * $max / 2;
+}
+echo implode(PHP_EOL, $ans);
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
