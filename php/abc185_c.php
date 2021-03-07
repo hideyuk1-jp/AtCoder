389472d8b6a3a13978d069686a6cb86ef93d6936
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-07 11:15:13 +0900

    abc185_c 茶diff。nCrするだけ

---
+++ php/abc185_c.php
@@ -0,0 +1,20 @@
+<?php
+const MOD = 2 ** 63;
+[$l] = ints();
+$nCr = comb($l - 1);
+echo $nCr[$l - 1][11];
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
+// nCr MODで余りを取っているので注意！
+function comb($max)
+{
+    $pascal[0][0] = 1;
+    for ($j = 1; $j <= $max; $j++) {
+        for ($i = 0; $i <= $j; $i++) {
+            $pascal[$j][$i] = (($pascal[$j - 1][$i - 1] ?? 0) + ($pascal[$j - 1][$i] ?? 0)) % MOD;
+        }
+    }
+    return $pascal;
+}
