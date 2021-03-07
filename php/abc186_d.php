0266a5ccfbe22d7593545e45ce89a8bd97f9549e
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-07 13:15:58 +0900

    abc186_d 茶diff。昇順に並び替えると絶対値考慮する必要ない

---
+++ php/abc186_d.php
@@ -0,0 +1,15 @@
+<?php
+[$N] = ints();
+$a = ints();
+sort($a);
+$sum = array_sum($a);
+$ans = 0;
+for ($i = 0; $i < $N - 1; ++$i) {
+    $sum -= $a[$i];
+    $ans += $sum - $a[$i] * ($N - $i - 1);
+}
+echo $ans;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
