90070a9ea219afc2564525f87d6f55be7be59c66
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-10 20:25:52 +0900

    灰diff（120）。これが120か。そこそこ難しいと思う

---
+++ php/abc188_c.php
@@ -0,0 +1,18 @@
+<?php
+[$n] = ints();
+$a = ints();
+$k = array_keys($a); // key記録
+for ($i = 0; $i < $n; $i++) {
+    for ($j = 0; $j < 2 ** $n; $j += 2 ** ($i + 1)) {
+        // 後ろの人が勝ったら前の人と入れ替え
+        if ($a[$j] < $a[$j + 2 ** $i]) {
+            [$a[$j], $a[$j + 2 ** $i]] = [$a[$j + 2 ** $i], $a[$j]];
+            [$k[$j], $k[$j + 2 ** $i]] = [$k[$j + 2 ** $i], $k[$j]];
+        }
+    }
+}
+echo $k[2 ** ($n - 1)] + 1;
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
