87880a3db308e176b6571cf5012f9db9b44eac97
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-11 19:34:39 +0900

    灰diffx2(21, 68)

---
+++ php/abc187_b.php
@@ -0,0 +1,13 @@
+<?php
+fscanf(STDIN, '%d', $n);
+for ($i  = 0; $i < $n; $i++) {
+    fscanf(STDIN, '%d %d', $x[], $y[]);
+}
+$cnt = 0;
+for ($i  = 0; $i < $n - 1; $i++) {
+    for ($j  = $i; $j < $n; $j++) {
+        $a = ($y[$i] - $y[$j]) / ($x[$i] - $x[$j]);
+        if (abs($a) <= 1) $cnt++;
+    }
+}
+echo $cnt;
---
+++ php/abc190_b.php
@@ -0,0 +1,8 @@
+<?php
+fscanf(STDIN, '%d %d %d', $n, $s, $d);
+$ans = 'No';
+for ($i  = 0; $i < $n; $i++) {
+    fscanf(STDIN, '%d %d', $x, $y);
+    if ($x < $s and $y > $d) $ans = 'Yes';
+}
+echo $ans;
