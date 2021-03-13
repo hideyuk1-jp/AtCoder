d59b0aca3b8c3a7fcd25e95bbb63f1e12c195501
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-13 20:53:05 +0900

    緑diff(1063)。解説見てAC。頂点判定覚えた

---
+++ php/abc191_c.php
@@ -0,0 +1,30 @@
+<?php
+define('WHITE', '.');
+define('BLACK', '#');
+[$H, $W] = ints();
+for ($i = 0; $i < $H; ++$i) {
+    [$S[]] = strs();
+}
+$cnt = 0;
+// 頂点判定
+for ($i = 0; $i < $H - 1; ++$i) {
+    for ($j = 0; $j < $W - 1; ++$j) {
+        $c = 0;
+        // 頂点か
+        if ($S[$i][$j] === BLACK) $c++;
+        if ($S[$i][$j + 1] === BLACK) $c++;
+        if ($S[$i + 1][$j] === BLACK) $c++;
+        if ($S[$i + 1][$j + 1] === BLACK) $c++;
+
+        $cnt += $c % 2;
+    }
+}
+echo $cnt;
+function strs()
+{
+    return explode(' ', trim(fgets(STDIN)));
+}
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
