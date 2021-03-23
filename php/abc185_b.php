8ad49f5a226e667af17666a6fcb413041159db5e
hideyuk1-jp <hh11028+github@gmail.com>
2021-03-23 23:28:14 +0900

    abc185_b

---
+++ php/abc185_b.php
@@ -0,0 +1,16 @@
+<?php
+fscanf(STDIN, '%d %d %d', $n, $m, $t);
+$_n = $n;
+$now = 0;
+$flag = 1;
+for ($i  = 0; $i < $m; $i++) {
+    fscanf(STDIN, '%d %d', $a, $b);
+    $_n -= $a - $now;
+    $flag &= $_n > 0;
+    $_n += $b - $a;
+    $_n = min($_n, $n);
+    $now = $b;
+}
+$_n -= $t - $now;
+$flag &= $_n > 0;
+echo $flag ? "Yes" : "No";
