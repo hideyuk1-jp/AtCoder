158f1c66ba1ac172a05c6f8ae268c34c247d2009
hideyuk1-jp <hh11028+github@gmail.com>
2021-04-11 19:08:16 +0900

    古い青diff。結局 親方向のノードの数=n-1-子方向のノードの和 に気付かず余分な計算してた

---
+++ php/arc028_c.php
@@ -0,0 +1,27 @@
+<?php
+fscanf(STDIN, '%d', $n);
+for ($i  = 1; $i < $n; $i++) {
+    fscanf(STDIN, '%d', $p);
+    $g[$i][] = $p;
+    $g[$p][] = $i;
+    $e[] = [$i, $p];
+}
+$ans = array_fill(0, $n, 0);
+dfs();
+echo implode(PHP_EOL, $ans), PHP_EOL;
+
+function dfs($parent = -1, $d = 0)
+{
+    global $n, $g, $ans;
+    // 子方向
+    $cnt = 1; // d の分
+    foreach ($g[$d] as $next) {
+        if ($parent === $next) continue;
+        $tmp = dfs($d, $next);
+        $cnt += $tmp;
+        $ans[$d] = max($ans[$d], $tmp);
+    }
+    // 親方向
+    $ans[$d] = max($ans[$d], $n - $cnt);
+    return $cnt;
+}
