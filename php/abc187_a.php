957b7c6088df66811927e84a1472c4879fe3ccb1
hideyuk1-jp <hh11028+github@gmail.com>
2021-06-29 08:35:43 +0900

    abc187_a はい きょむ

---
+++ php/abc187_a.php
@@ -0,0 +1,16 @@
+<?php
+[$a, $b] = ints();
+echo max(f($a), f($b));
+function f($n)
+{
+    $s = strval($n);
+    $ret = 0;
+    for ($i = 0; $i < strlen($s); ++$i) {
+        $ret += intval($s[$i]);
+    }
+    return $ret;
+}
+function ints()
+{
+    return array_map('intval', explode(' ', trim(fgets(STDIN))));
+}
